a42817acfee17f38c232090172073e717b5e8a1d
kimkunyou <bbaro1435@gmail.com>
2023-01-12 15:55:32 +0900

    countdown 02

--- k_software/day01.py
+++ k_software/day01.py
@@ -1,12 +1,15 @@
-#(100°F − 32) × 5/9 = 37.778°C
+import time
+#snake : '_' 의 사용 -> 평소 내가 쓰던 방식.
+#cammel : 단어가 달라질 때 대문자를 사용하는거 -> 김승환 교수님이 쓰던 방식
+countdown_snake = [5,4,3,2,1,'Hey!']
+countdownCammel = [5,4,3,2,1,'Hey!']
 
-# def temp_converter(f):
-#     c = (100*f-32)*(5/9)
-#     return c
-#
-# f = float(input('화씨 온도 : '))
-# print(temp_converter(f))
+for countdown in 5,4,3,2,1,'Hey!':
+    #time.sleep(1)
+    print(countdown)
 
-fahrenheit = float(input('화씨 온도 : '))
-celsius = (fahrenheit - 32.0) * (5.0/9.0)
-print(f'화씨 온도 {fahrenheit}도는 섭씨 온도 {celsius}입니다.')
+print(countdown_snake[3])
+print(countdown_snake[-1])
+
+print('program over')
+
