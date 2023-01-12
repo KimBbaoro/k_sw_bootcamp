8c7bd07b053f68163c8397e9e2db353d4ea008b7
kimkunyou <bbaro1435@gmail.com>
2023-01-12 15:57:14 +0900

    gui 04

--- k_software/day01.py
+++ k_software/day01.py
@@ -1,18 +1,6 @@
-example ={'korean' : 'Kimchi', 'japan' : "sushi", 'china' : "duck"}
+import tkinter as tk
 
-
-country = 'japan'
-print(f'{country}의 맛있는 음식은 {example[country]}')
-
-#old style
-print('%s의 맛있는 음식은 %s _ v'%(country, example[country]))
-print("{0}의 맛있는 음식은 {1} oldest v".format(country, example[country]))
-
-
-import webbrowser
-import json
-from urllib.request import urlopen
-
-# print("Let's find an old website.")
-# site = input("Type a website URL : ")
-# era = input("Type a")
+win = tk.Tk()
+win.geometry('400x300')
+win.title('파이썬 1일차 Preview')
+win.mainloop()
